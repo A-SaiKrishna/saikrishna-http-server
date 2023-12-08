1541250dca1e1b431654bb3e697edf489cb967a2
saikrishna <saikrishnaarevarapu@gmail.com>
2023-12-08 16:45:27 +0530

    server using express

--- index.js
+++ index.js
@@ -1,76 +1,41 @@
 const http = require("http");
+const express = require("express");
 const fs = require("fs");
 const crypto = require("crypto");
 const jsobj = require("./jsobj");
+const path = require("path");
 
-function readingFile() {
-  return new Promise((res, rej) => {
-    fs.readFile("./sample.html", "utf-8", (err, data) => {
-      if (err) {
-        rej(err);
-      } else {
-        res(data);
-      }
-    });
-  });
-}
+const app = express();
 
-let server = http.createServer((req, res) => {
-  //GET /html - Should return the following HTML content.
-  // Note when opened in the browser it should display the HTML page and not the HTML code.
-  if (req.url === "/html" && req.method === "GET") {
-    readingFile().then((data) => {
-      res.writeHeader(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-  }
-  //GET /json - Should return the following JSON string
-  if (req.url === "/json" && req.method === "GET") {
-    res.write(JSON.stringify(jsobj));
-    res.end();
-  }
-  //GET /uuid - Should return a UUID4. For example:
-  if (req.url === "/uuid" && req.method === "GET") {
-    let uuid = crypto.randomUUID();
-    res.write(JSON.stringify({ uuis: uuid }));
-    res.end();
-  }
-  let arr = [100, 200, 300, 400, 500];
-  /**GET /status/{status_code} - Should return a response with a status code as specified in the request. For example:
-
-        /status/100 - Return a response with 100 status code
-        /status/500 - Return a response with 500 status code */
-  arr.forEach((element) => {
-    if (req.url === `/status/${element}` && req.method === "GET") {
-      res.writeHead(element);
-      //res.write();
-      res.end(JSON.stringify(res.statusCode));
-    }
-  });
-  /**GET /delay/{delay_in_seconds} - Should return a success response but after the specified delay in the request.
-   *  For example: If the request sent is GET /delay/3,
-   * then the server should wait for 3 seconds and only then send a response with 200 status code. */
-  if (req.url.includes("delay") && req.method === "GET") {
-    res.writeHead(200);
-    let arr = req.url.split("/");
-    //console.log(arr);
-    let times = Number(arr[arr.length - 1]);
-    //console.log(times);
-    //console.log(typeof times);
-    if (typeof times === "number") {
-      let sam = new Promise((res, rej) => {
-        setTimeout(() => {
-          res();
-        }, times * 1000);
-      });
-      sam.then(() => {
-        res.write(JSON.stringify(res.statusCode));
-        res.end();
-      });
-    }
-    //res.end()
-  }
+// app
+// problem1
+app.get("/html", (req, res) => {
+  res.sendFile(path.join(__dirname, "sample.html"));
+  res.end();
 });
 
-server.listen(8000);
+//problem2
+app.get("/json", (req, res) => {
+  res.json(jsobj);
+  res.end();
+});
+app.get("/status/:id", (req, res) => {
+  if (typeof Number(req.params.id) === "number") {
+    res.send(req.params.id);
+    res.status(Number(req.params.id));
+  } else {
+    res.send("error");
+  }
+  res.end();
+});
+app.get("/delay/:sec", (req, res) => {
+  let ti = Number(req.params.sec);
+  if (typeof ti === "number") {
+    setTimeout(() => {
+      res.send(res.statusCode);
+    }, ti * 1000);
+  }
+});
+app.listen(8000, () => {
+  console.log("listening at port 8000");
+});
